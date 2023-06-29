41abce3d6673bb8f369092bfbe1ce27253e7e943
Matheus leão <kyracraft0@gmail.com>
2023-06-29 14:14:48 -0300

    Criados Elementos de SoftSkills e componente de icones

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,23 +1,7 @@
 import Art from '../Assets/Art.png';
 import Habilidades from '../Assets/Habilidades.png';
-import js from '../Icons/js.png';
-import typescript from '../Icons/typescript.png';
-import python from '../Icons/python.png';
-import html from '../Icons/html.png';
-import css from '../Icons/css.png';
-import docker from '../Icons/docker.png';
-import atom from '../Icons/atom.png';
-import redux from '../Icons/redux.png';
-import test from '../Icons/test.png';
-import git from '../Icons/git.png';
-import github from '../Icons/github.png';
-import npm from '../Icons/npm.png';
-import netifly from '../Icons/netifly.png';
-import expressJs from '../Icons/expressJs.webp';
-import mysql from '../Icons/mysql.png';
-import mongoDB from '../Icons/mongoDB.png';
-import figma from '../Icons/figma.png';
-import linux from '../Icons/linux.png';
+import Icons from './Icons';
+import SoftCards from './SoftCards';
 
 function Skills() {
   return (
@@ -28,61 +12,8 @@ function Skills() {
 
       </section>
 
-      <section className="skills">
-        <img width="60px" alt="JavaScript Icon" src={js} />
-        JavaScript
-
-        <img width="60px" alt="TypeScript Icon" src={typescript} />
-        TypeScript
-
-        <img width="60px" alt="Python Icon" src={python} />
-        Python
-
-        <img width="60px" alt="HTML Icon" src={html} />
-        HTML
-
-        <img width="60px" alt="CSS Icon" src={css} />
-        CSS
-
-        <img width="60px" alt="Docker Icon" src={docker} />
-        Docker
-
-        <img width="60px" alt="React.js Icon" src={atom} />
-        React.js
-
-        <img width="60px" alt="Redux Icon" src={redux} />
-        Redux
-
-        <img width="60px" alt="Testing Library Icon" src={test} />
-        Testing Library
-
-        <img width="60px" alt="Git Icon" src={git} />
-        Git
-
-        <img width="60px" alt="GitHub Icon" src={github} />
-        GitHub
-
-        <img width="60px" alt="NPM Icon" src={npm} />
-        NPM
-
-        <img width="60px" alt="Netlify Icon" src={netifly} />
-        Netlify
-
-        <img width="60px" alt="ExpressJs Icon" src={expressJs} />
-        ExpressJs
-
-        <img width="60px" alt="MySQL Icon" src={mysql} />
-        MySQL
-
-        <img width="60px" alt="MongoDB Icon" src={mongoDB} />
-        MongoDB
-
-        <img width="60px" alt="Figma Icon" src={figma} />
-        Figma
-
-        <img width="60px" alt="Linux Icon" src={linux} />
-        Linux
-      </section>
+      <Icons />
+      <SoftCards />
     </div>
   );
 }
---
+++ src/Components/SoftCards.jsx
@@ -0,0 +1,48 @@
+function SoftCards() {
+  return (
+    <section className="softSkills">
+
+      <div className="softCard">
+        Criatividade
+        <p className="softText">
+          The Last of Us is a television series based on the
+          popular video game of the same name. It follows the
+          story of Joel and Ellie, two survivors of a
+          post-apocalyptic world ravaged by a deadly fungus
+          that turns people into zombies, as they navigate
+          through dangerous terrain and encounter other survivors.
+
+        </p>
+      </div>
+
+      <div className="softCard">
+        Comunição
+        <p className="softText">
+          The Last of Us is a television series based on the
+          popular video game of the same name. It follows the
+          story of Joel and Ellie, two survivors of a
+          post-apocalyptic world ravaged by a deadly fungus
+          that turns people into zombies, as they navigate
+          through dangerous terrain and encounter other survivors.
+
+        </p>
+      </div>
+
+      <div className="softCard">
+        Curva de Aprendizado
+        <p className="softText">
+          The Last of Us is a television series based on the
+          popular video game of the same name. It follows the
+          story of Joel and Ellie, two survivors of a
+          post-apocalyptic world ravaged by a deadly fungus
+          that turns people into zombies, as they navigate
+          through dangerous terrain and encounter other survivors.
+
+        </p>
+      </div>
+
+    </section>
+  );
+}
+
+export default SoftCards;

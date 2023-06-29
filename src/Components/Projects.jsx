bb2937430de4a5079b3ed9169f3daad7db971616
Matheus leão <kyracraft0@gmail.com>
2023-06-29 12:24:15 -0300

    Criado Componente dos projetos e upload da telas dos projetos

---
+++ src/Components/Projects.jsx
@@ -0,0 +1,20 @@
+import Projetos from '../Assets/Projetos.png';
+import arrow from '../Assets/arrow.png';
+import Baratiê from '../Images/Baratiê.png';
+
+function Projects() {
+  return (
+    <div className="projectsPage">
+      <img alt="Projetos" src={Projetos} />
+      <section className="projects">
+        <img width="100px" alt="seta" src={arrow} />
+        <img width="1000px" alt="Baratiê" src={Baratiê} />
+        <img width="100px" alt="seta" src={arrow} />
+
+      </section>
+
+    </div>
+  );
+}
+
+export default Projects;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import About from '../Components/About';
+import NavBar from '../Components/NavBar';
+import Projects from '../Components/Projects';
+import Skills from '../Components/Skills';
 
 function Home() {
   return (
-    <h1>Portfolio</h1>
+    <div>
+      <NavBar />
+      <About />
+      <Skills />
+      <Projects />
+
+      <a href="https://www.flaticon.com/free-icons/react" title="react icons">React icons created by Freepik - Flaticon</a>
+    </div>
   );
 }
 

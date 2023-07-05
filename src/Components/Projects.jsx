cceada854cfd54249c0a4a9be7bb8f6d4611e95c
Matheus leão <kyracraft0@gmail.com>
2023-07-05 17:01:53 -0300

    Criado o funcionamento da tela de projetos

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,23 +1,9 @@
 /* eslint-disable react/prop-types */
-import { useState } from 'react';
 import Projetos from '../Assets/Projetos.png';
 import Project from '../AssetsENG/Projects.png';
-import arrow from '../Assets/arrow.png';
-import Baratiê from '../Images/Baratiê.png';
+import ProjectsMap from './ProjectsMap';
 
 function Projects({ language }) {
-  const [filter, setFilter] = useState('projectImage');
-  const [infoFilter, setInfoFilter] = useState('projectInfos');
-
-  const blur = () => {
-    if (filter === 'projectImage' || infoFilter === 'projectInfo') {
-      setFilter('projectBlur');
-      setInfoFilter('infoVisible');
-    } else {
-      setFilter('projectImage');
-      setInfoFilter('projectInfos');
-    }
-  };
   return (
     <div id="Projects" className="projectsPage">
 
@@ -26,41 +12,7 @@ function Projects({ language }) {
           : <img className="projectsName" alt="Projetos" src={Project} />}
       </div>
 
-      <div className="projectsDiv">
-        <button className="arrowButton" type="button">
-          <img className="leftArrow" alt="seta" src={arrow} />
-
-        </button>
-
-        <section onMouseEnter={blur} onMouseLeave={blur} className="projects">
-
-          <img className={filter} alt="Baratiê" src={Baratiê} />
-
-          <div className={infoFilter}>
-
-            <h1 className="projectTitle">Baratiê Recipes App</h1>
-            <h3 className="projectDesc">
-              Um Aplicativo Mobile com diferentes
-              receitas de comida e bebidas
-            </h3>
-
-            <h4 className="projectTasks">React.js</h4>
-            <h4 className="projectTasks">JavaScript</h4>
-            <h4 className="projectTasks">UI & UX</h4>
-            <h4 className="projectTasks">CSS</h4>
-
-            <br />
-
-            <button type="button" className="projectButtons">Repositorio</button>
-            <button type="button" className="projectButtons">Veja o Site</button>
-          </div>
-        </section>
-        <button className="arrowButton" type="button">
-          <img className="arrow" alt="seta" src={arrow} />
-
-        </button>
-
-      </div>
+      <ProjectsMap />
 
     </div>
   );
---
+++ src/Utils/AllProjects.js
@@ -0,0 +1,29 @@
+const projectsBR = [{
+  name: 'Baratiê',
+  describe: 'Um Aplicativo Mobile com diferentes receitas de comida e bebidas',
+  skills: ['React.js', 'JavaScript', 'UI & UX', 'CSS'],
+}, {
+  name: 'Trybe Clube de Futebol',
+  describe: 'Uma aplicação com acesso a partidas de futebol, todo o front-end foi desenvolvido pela Trybe e o Back-end feito por mim',
+  skills: ['Node.js', 'TypeScript', 'POO', 'Unit Tests'],
+}, {
+  name: 'Trybeer',
+  describe: 'Uma aplicativo de entrega de bebidas, onde você pode fazer pedidos de diferentes bebidas e acompanhar as entregas',
+  skills: ['Node.js', 'JavaScript', 'Redux', 'Unit Tests'],
+}];
+
+const projectsENG = [{
+  name: 'Baratiê',
+  describe: 'A Mobile website with different foods and drinks recipes',
+  skills: ['React.js', 'JavaScript', 'UI & UX', 'CSS'],
+}, {
+  name: 'Trybe Football Club',
+  describe: 'A website',
+  skills: ['Node.js', 'TypeScript', 'POO', 'Unit Tests'],
+}, {
+  name: 'Trybeer',
+  describe: 'Uma aplicativo de entrega de bebidas, onde você pode fazer pedidos de diferentes bebidas e acompanhar as entregas',
+  skills: ['Node.js', 'JavaScript', 'Redux', 'Unit Tests'],
+}];
+
+export { projectsBR, projectsENG };
